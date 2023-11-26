58ead4b241884b8168c0699eb4becb297cbdfee4
Rasmus Heikkinen <rasmus.heikkinen@helsinki.fi>
2023-11-26 16:05:46 +0200

    Change date types to strings and fix a typo

--- src/types.ts
+++ src/types.ts
@@ -31,25 +31,25 @@ export enum EntryType {
   Hospital = 'Hospital',
 }
 
-interface HealthCheckEntry extends BaseEntry {
+export interface HealthCheckEntry extends BaseEntry {
   type: 'HealthCheck';
   healthCheckRating: HealthCheckRating;
 }
 
-interface OccupationalHealthcareEntry extends BaseEntry {
+export interface OccupationalHealthcareEntry extends BaseEntry {
   type: 'OccupationalHealthcare';
   employerName: string;
-  sickLeave: {
-    startDate: Date;
-    endDate: Date;
+  sickLeave?: {
+    startDate: string;
+    endDate: string;
   };
 }
 
-interface HospitalEntry extends BaseEntry {
+export interface HospitalEntry extends BaseEntry {
   type: 'Hospital';
   discharge: {
-    date: Date;
-    citeria: string;
+    date: string;
+    criteria: string;
   };
 }
 
